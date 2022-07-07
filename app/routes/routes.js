39a8d74c7ef7ded1e45dee4e22ab75df74616402
Bas Voss <Voss.bas@gmail.com>
2022-07-07 21:14:06 +0200

    initial user setup

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const contactRoutes = require('./contact.route')
+const userRoutes = require('./user.route')
 
 router.use('/contacts', contactRoutes)
+router.use('/user', userRoutes)
 
 module.exports = router
